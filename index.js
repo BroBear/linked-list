86d3131ba5599aff79ee2377fa740ebc61d5d9c6
Megan Brown <stockbtcrew@gmail.com>
2016-12-13 09:00:02 -0700

    Made URL anchor and add counters

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ function buttonEnabler() {
     $('.enter-btn').attr('disabled', false);
   }
 };
+function totalLinks() {
+  var count = $(".stored-site").length;
+  $('.links').text("Number of Links: "+ count);
+}
+function readLinks() {
+  var count = $(".read").length;
+  $('.read-links').text("Number of Read Links: "+ count);
+}
+function unreadLinks() {
+  var totalBookmarks = $(".stored-site").length;
+  var readBookmarks = $(".read").length;
+  var unreadTotal = totalBookmarks - readBookmarks;
+  $('.unread-links').text("Number of Unread Links: "+ unreadTotal);
 
+}
 // Creates bookmark properties
 function CreateBookmark (webTitle, webUrl) {
   this.title = webTitle;
@@ -16,10 +30,10 @@ function CreateBookmark (webTitle, webUrl) {
 
 // Creates bookmark content and html
 function bookmarkContent(createBookmark) {
-  $( ".bookmarks" ).prepend(`
+  $(".bookmarks").prepend(`
     <article class="stored-site">
       <h5>${createBookmark.title}</h5>
-      <p class="unread">${createBookmark.url}</p>
+      <p class="unread"><a href="${createBookmark.url}" target="_blank">http://${createBookmark.url}</a></p>
       <input type="submit" value="Read" id="read-btn">
       <input type="submit" value="Delete" id="delete-btn">
     </article>`
@@ -40,6 +54,11 @@ function resetInputFields() {
   $( ".web-url" ).val('');
 }
 
+//need to make clear button
+$( ".bookmarks" ).on("click", "#xxxxx-btn", function() {
+  $(".stored-site").remove();
+    });
+
 //binds the enter button to typing in inputfields
 $( ".web-title, .web-url" ).keyup(function() {
   buttonEnabler();
@@ -49,6 +68,8 @@ $( ".web-title, .web-url" ).keyup(function() {
 $( ".enter-btn" ).click(function() {
   makeBookmark();
   resetInputFields();
+  totalLinks()
+  unreadLinks()
   buttonEnabler();
 });
 
@@ -60,6 +81,8 @@ $( ".bookmarks" ).on("click", "#delete-btn", function() {
 // Toggle Class for Read Button
 $( ".bookmarks" ).on("click", "#read-btn", function() {
      $(this).toggleClass('read');
+     readLinks()
+     unreadLinks()
   });
 
 // // Use Enter to submit
